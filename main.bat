82ddf0c1f337f799f5defc4203f39d5d83e3ade9
AndoxYap_Ideagen <andox.yap@ideagenplc.com>
2024-06-19 11:21:18 +0800

    Updated main.bat

--- main.bat
+++ main.bat
@@ -1,13 +1,12 @@
 @echo off
-
 set current_dir="%CD%"
-set "openjdk_url=https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
-set "jenkins_agent_url=https://github.com/winsw/winsw/releases/download/v2.11.0/WinSW-x64.exe"
+@REM set "openjdk_url=https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_windows-x64_bin.zip"
+set "jenkins_agent_url=https://github.com/winsw/winsw/releases/download/v2.12.0/WinSW-x64.exe"
 
-:download_jdk11
-rem jdk-11.0.2
-curl -o "%current_dir%\openjdk-11.0.2_windows-x64_bin.zip" "%openjdk_url%"
-tar -xf openjdk-11.0.2_windows-x64_bin.zip
+@REM :download_jdk11
+@REM rem jdk-11.0.2
+@REM curl -o "%current_dir%\openjdk-11.0.2_windows-x64_bin.zip" "%openjdk_url%"
+@REM powershell -Command "Expand-Archive -Path '%current_dir%\openjdk-11.0.2_windows-x64_bin.zip' -DestinationPath '%current_dir%'"
 
 :download_agent
 curl -o "%current_dir%\jenkins-agent.exe" "%jenkins_agent_url%"
